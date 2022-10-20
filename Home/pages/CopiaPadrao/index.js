a200709c3fcab7b944efdd245cfc015cb4e4cd04
Mateus Alves <100079925+Mateusalves11@users.noreply.github.com>
2022-10-20 20:33:00 -0300

    Add files via upload

---
+++ Home/pages/CopiaPadrao/index.js
@@ -0,0 +1,6 @@
+  function SidebarActive() {
+    document.getElementById("sidebar").classList.toggle("sidebaractive");
+  }
+  function ButtonActive() {
+    document.getElementById("content").classList.toggle("contentactive");
+  }
---
+++ Home/pages/PerfilProfe/index.js
@@ -0,0 +1,9 @@
+function SidebarActive() {
+  document.getElementById("sidebar").classList.toggle("sidebaractive");
+}
+function ButtonActive() {
+  document.getElementById("content").classList.toggle("contentactive");
+  document.getElementById("content1").classList.toggle("contentactive");
+  document.getElementById("content2").classList.toggle("contentactive");
+  document.getElementById("content3").classList.toggle("contentactive");
+}
